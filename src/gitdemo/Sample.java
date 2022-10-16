0c209f61ba3df220d761ab3a44ac4062bae95ad5
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-16 21:58:25 +0530

    msg updated

--- src/gitdemo/Sample.java
+++ src/gitdemo/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("CI/CD USING DEVOPS");
+		System.out.print("CI/CD USING DEVOPS...");
 	}
 
 }
